ae9bb1fbd503d45226297a6dbab97d018ccb6bd0
yuryvn <yury.v.n@gmail.com>
2016-10-04 19:23:45 +0200

    starts ex 6.7

--- chap6/exercises.cpp
+++ chap6/exercises.cpp
@@ -54,3 +54,49 @@ void TComplex::Minus( TComplex &Var1,  TComplex &Var2){
 void TComplex::PrintNumber(){
 	cout << "Complex Number is " << "(" << Re1 << "," << Im1 << ")" << endl;
 }
+
+//ex 3.7 rational class
+
+
+TRational::TRational(int A_init = 1, int B_init = 1){ SetNumber(A_init,B_init); }
+
+TRational::~TRational(){}
+
+
+void TRational::ReduceNumber(){
+	for(int i=1;i<
+}
+
+
+template <class InitClass>
+
+void TRational::SetNumber(InitClass A_init, InitClass B_init){
+	while (((int)A_init != A_init){
+		cout << "A must be integer, reenter A:";
+		cin >> A_init;
+	}
+	a = (int)A_init;
+
+	while (((int)B_init != B_init||B_init==0){
+		cout << "B must be integer and not equal to 0, reenter B:";
+		cin >> B_init;
+	}
+	b = (int)B_init;
+	ReduceNumber();
+}
+
+void TRational::PrintNormal(){
+	cout << a << "/" << b << endl;
+}
+
+void TRational::PrintFloat(){
+	double out = (double)a / (double)b;
+	cout << out << endl;
+}
+
+
+void Sum(TRational  &Var1, TRational  &Var2);
+void Minus(TRational &Var1, TRational &Var2);
+void Mult(TRational  &Var1, TRational  &Var2);
+void Del(TRational &Var1, TRational &Var2);
+
--- chap6/exercises.h
+++ chap6/exercises.h
@@ -21,3 +21,24 @@ private:
 	void Minus_private();
 };
 
+//ex 6.7 Rational numbers operations
+class TRational
+{
+public:
+	TRational(int A_init = 1, int B_init = 1);
+	~TRational();
+	template <class InitClass>
+	void SetNumber(InitClass A_init, InitClass B_init);
+
+	void PrintNormal();
+	void PrintFloat();
+	void Sum(TRational  &Var1, TRational  &Var2);
+	void Minus(TRational &Var1, TRational &Var2);
+	void Mult(TRational  &Var1, TRational  &Var2);
+	void Del(TRational &Var1, TRational &Var2);
+
+
+private:
+	int a, b;
+	void ReduceNumber();
+};
